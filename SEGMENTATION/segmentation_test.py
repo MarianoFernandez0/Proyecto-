443f1c9593e47011db4b2ce358587ff5b08d3723
leo <leonardo.de.los.santos280796@gmail.com>
2020-01-25 21:55:22 -0300

    Updated: 25/01/2020 21:55:03. Segmentacion terminada. Falta probarla

--- SEGMENTATION/segmentation_test.py
+++ SEGMENTATION/segmentation_test.py
@@ -1,20 +1,23 @@
 #!/home/leo/anaconda3/bin/python
-import numpy as np
-import matplotlib.pyplot as plt
 import skimage.data as data
-import skimage.segmentation as seg
 import skimage.filters as filters
-import skimage.draw as draw
 import skimage.color as color
+import scipy.ndimage as nd
 
 
-def image_show(image, nrows=1, ncols=1, cmap='gray'):
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(14, 14))
-    ax.imshow(image, cmap='gray')
-    ax.axis('off')
-    return fig, ax
-
-
-text = data.page()
-image_show(text)
-
+def segmentation(image):
+    '''
+    Segmenta una imagen dada.
+    En el estudio para el proyecto, se definió
+    que filtrar la imagen y luego segmentar utilizando OTSU
+    fue el mejor desempeño
+    Entradas:
+        image: imagen de tamaño NxMx3 la cual se desea segmentar
+    Salida:
+        Máscara de tamaño NxMx1 con la imagen segmentada
+    '''
+    image_bw = color.rgb2gray(image)
+    image_filtered = nd.gaussian_filter(image_bw, sigma=3)
+    image_threshold = filters.threshold_otsu(image_filtered)
+    mask = image_bw > image_threshold
+    return mask
