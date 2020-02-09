cdc964bce48a117c8ff4a721f7416c78589d1c62
Leonardo de los Santos <leonardodelossantos@Leonardos-MacBook-Air.local>
2020-02-09 12:56:17 -0300

    r

--- project/ground_truth/generador_simulaciones_2.py
+++ project/ground_truth/generador_simulaciones_2.py
@@ -1,19 +1,16 @@
-
 from matplotlib import pyplot as plt
-from skimage.io import imread
-from skimage.external.tifffile import imsave
+from skimage.io import imread, imsave
 import numpy as np
-from skimage.draw import ellipse
+import cv2
 import math
 
 plt.rcParams['figure.figsize'] = (20.0, 15.0)
 plt.rcParams['image.cmap'] = 'gray'
 
 
-def generate_sequence(M=512, N=512, frames=250, mean=[10, 5],
-	cov =[[103, 21], [21, 14]], vm=3, sigma_v=3, sigma_theta=10,
-	particles=500, sigma_r=1, add_to_sequence=False):
-	
+def generate_sequence(M = 512, N = 512, frames = 250, mean = [10, 5],
+	cov = [[103, 21], [21, 14]], vm = 3, sigma_v = 3, sigma_theta = 10,
+	particles = 500, sigma_r = 1, add_to_sequence = False):
 	'''
 	Genera una secuencia simulada con las características dadas y la guarda en la carpeta "Simulated".
 	Parametros:
@@ -38,39 +35,56 @@ def generate_sequence(M=512, N=512, frames=250, mean=[10, 5],
 	x = np.zeros([particles, frames])
 	y = np.zeros([particles, frames])
 
-	x[:,0] = np.random.uniform(-M, 2*M, particles)                # Posición inicial de las partículas
-	y[:,0] = np.random.uniform(-N, 2*N, particles)    
+	x[:, 0] = np.random.uniform(-M, 2 * M, particles)                # Posición inicial de las partículas
+	y[:, 0] = np.random.uniform(-N, 2 * N, particles)    
 
 	d = np.random.multivariate_normal(mean, cov, particles)       # Se inicializa el tamaño de las partículas
-	a = d[:,0]
-	l = d[:,1]
+	a = d[:, 0]
+	l = d[:, 1]
 
-	theta = np.random.uniform(0,360,particles)       # Ángulo inicial 
-	v = np.random.normal(vm,10,particles)            # Velocidad inicial
+	theta = np.random.uniform(0, 360, particles)       # Ángulo inicial 
+	v = np.random.normal(vm, 10, particles)            # Velocidad inicial
 
 	for f in range(frames):                          # Se crean los cuadros de a uno 
-	    if f>0:
-	        x[:,f] = x[:,f-1]+v*np.cos(np.radians(theta))
-	        y[:,f] = y[:,f-1]+v*np.sin(np.radians(theta))
-	    #name = 'Simulated\imagen' + str(f) + '.png'
+	    if f > 0:
+	        x[:, f] = x[:, f - 1] + v * np.cos(np.radians(theta))
+	        y[:, f] = y[:, f - 1] + v * np.sin(np.radians(theta))
+	    name = 'Simulated\imagen' + str(f) + '.png'
 	    if add_to_sequence:
 	        Iaux = imread(name)
 	    else:
 	        Iaux = I.copy()
 
 	    for p in range(particles):                  					# Se agregan las partículas a la imágen de a una
-	        rr, cc = ellipse(x[p,f], y[p,f], l[p], a[p], I.shape,np.radians(theta[p])-math.pi/2)
+	        ellipse_outer = ((x[p, f], y[p, f]), (l[p]*2, a[p]*2), math.degrees(theta[p]))
+	        Iaux = cv2.ellipse(transparency, ellipse_outer, 255, -1, cv2.LINE_AA)
+
+	        rr, cc = ellipse(x[p, f], y[p, f], l[p], a[p], I.shape,np.radians(theta[p]) - math.pi / 2)
 	        Iaux[rr,cc] = 255
 	                
 	    Inoise = Iaux + np.random.normal(0,sigma_r,Iaux.shape) 			# Se agrega ruido a las imágenes
 
-	    imsave(name,np.uint8(np.round(((Inoise-np.min(Inoise))/(np.max(Inoise)-np.min(Inoise))*255))))
-	    v = np.abs(np.random.normal(v,sigma_v,particles))     #Próximo paso  
-	    
-	    theta = np.random.normal(theta,sigma_theta,particles)      
-	return x,y
+	    imsave(name, np.uint8(np.round(((Inoise - np.min(Inoise)) / (np.max(Inoise) - np.min(Inoise)) * 255 ))))
+	    v = np.abs(np.random.normal(v, sigma_v,particles))     #Próximo paso  
+	    theta = np.random.normal(theta, sigma_theta, particles)      
+	return x, y
 
-def velocity(M,N,x,y):
+
+#Hago el gradiente para que quede con un brillo parecido al de las imágenes de fmed
+def make_gradient_v(width, height, h, k, a, b, theta):
+    # Precalculate constants
+    st, ct =  math.sin(theta), math.cos(theta)
+    aa, bb = a**2, b**2
+
+    # Generate (x,y) coordinate arrays
+    y,x = np.mgrid[-k:height-k,-h:width-h]
+    # Calculate the weight for each pixel
+    weights = (((x * ct + y * st) ** 2) / aa) + (((x * st - y * ct) ** 2) / bb)
+
+    return np.clip(1.0 - weights, 0, 1)
+
+
+def velocity(M, N, x, y):
 	"""
 	Calcula la velocidad intantánea para cada partícula y cada cuadro de la secuencia.
 
@@ -86,13 +100,13 @@ def velocity(M,N,x,y):
 	vel = np.zeros(x.shape)
 	for p in range(x.shape[0]):
 	    for f in range(1,x.shape[1]):
-	        if (x[p,f]>0 and x[p,f]<M) and (y[p,f]>0 and y[p,f]<N):
-	            vel[p,f] = np.sqrt((x[p,f-1]-x[p,f])**2+(y[p,f-1]-y[p,f])**2)
+	        if (x[p, f] > 0 and x[p, f] < M) and (y[p, f] > 0 and y[p, f] < N):
+	            vel[p, f] = np.sqrt((x[p, f - 1] - x[p, f])**2 + (y[p, f - 1] - y[p, f])**2)
 	        else: 
-	            vel[p,f] = None
+	            vel[p, f] = None
 	return vel
 
-def total_distance(M,N,x,y):
+def total_distance(M, N, x, y):
 	"""
 	Calcula la distancia recorrida en toda la secuencia por cada particula (solo toma en cuenta cuando la partícula está en la imágen).
 
@@ -108,9 +122,9 @@ def total_distance(M,N,x,y):
 	"""
 	dis = np.zeros(x.shape[0])
 	for p in range(x.shape[0]):
-	    for f in range(1,x.shape[1]):
-	        if (x[p,f]>0 and x[p,f]<M) and (y[p,f]>0 and y[p,f]<N):
-	            dis[p] = dis[p] + np.sqrt((x[p,f-1]-x[p,f])**2+(y[p,f-1]-y[p,f])**2)
+	    for f in range(1, x.shape[1]):
+	        if (x[p, f] > 0 and x[p, f] < M) and (y[p, f] > 0 and y[p, f] < N):
+	            dis[p] = dis[p] + np.sqrt((x[p, f - 1] - x[p, f])**2 + (y[p, f - 1] - y[p, f])**2)
 	return dis
 
 def mean_velocity(vel):
@@ -126,26 +140,26 @@ def mean_velocity(vel):
 	"""
 	vel_m = np.zeros(vel.shape[0])
 	for p in range(vel.shape[0]):
-	    count=0
+	    count = 0
 	    for f in range(vel.shape[1]):
-	        if vel[p,f] != None and (math.isnan(vel[p,f])==False):
-	            vel_m[p] = vel_m[p] + vel[p,f]
-	            count = count+1
-	    vel_m[p] = vel_m[p]/count
+	        if vel[p, f] != None and (math.isnan(vel[p, f]) == False):
+	            vel_m[p] = vel_m[p] + vel[p, f]
+	            count = count + 1
+	    vel_m[p] = vel_m[p] / count
 	return vel_m
 
-mean = np.array([20.7247332,9.61818939])
-cov = np.array([[103.80124818,21.61793687],
-				 [ 21.61793687,14.59060681]])
-x,y= generate_sequence(frames=40,sigma_r=10,particles=100,mean=mean,cov=cov)
+mean = np.array([20.7247332, 9.61818939])
+cov = np.array([[103.80124818, 21.61793687],
+				 [ 21.61793687, 14.59060681]])
+x, y= generate_sequence(frames = 40, sigma_r = 10, particles = 100, mean = mean, cov = cov)
 
 
-#mean = np.array([10,5])
-#cov = np.array([[103.80124818,21.61793687],
-#[ 21.61793687,14.59060681]])
-#x,y= genetate_sequence(frames=40,sigma_r=10,particles=100,mean=mean,cov=cov,add_to_sequence=True,vm=7,sigma1=1,sigma2=2)
+#mean = np.array([10, 5])
+#cov = np.array([[103.80124818, 21.61793687],
+#[ 21.61793687, 14.59060681]])
+#x, y= genetate_sequence(frames = 40, sigma_r = 10, particles = 100, mean = mean, cov = cov, add_to_sequence = True, vm = 7, sigma1 = 1, sigma2 = 2)
 
-#vel = velocity(x,y,512,512)
+#vel = velocity(x, y, 512, 512)
 #vel_m = mean_velocity(vel)
-#dis = total_distance(x,y,512,512)
+#dis = total_distance(x, y, 512, 512)
 
