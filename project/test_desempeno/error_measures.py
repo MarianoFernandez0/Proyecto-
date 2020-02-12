208f02ab600b74101952a1f2ed52b46b327d5dfc
MarianoFernandez0 <mariano.ftr@gmail.com>
2020-02-12 19:11:02 -0300

    Test 0.0

---
+++ project/test_desempeno/error_measures.py
@@ -0,0 +1,14 @@
+
+def error_measures(X, Y):
+	'''
+	Parametros:
+		X (numpy.array((particles,frames))): Coordenadas ground truth de las partículas.
+		Y (numpy.array((particles,frames))): Coordenadas medidas de las partículas.
+
+	Return:
+		TP:
+		FN:
+		FP:
+		JSC:
+	
+	
