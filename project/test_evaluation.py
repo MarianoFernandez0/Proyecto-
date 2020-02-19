4fdc6223a39693aeb22c5fb498afe21e4155dba1
MarianoFernandez0 <mariano.ftr@gmail.com>
2020-02-18 22:06:05 -0300

    actualizaciones

--- project/test_evaluation.py
+++ project/test_evaluation.py
@@ -3,7 +3,8 @@ import time
 from evaluation import evaluation
 import numpy as np
 from ground_truth.generador_simulaciones_2 import generate_sequence
-from test_desempeno.error_measures import error_measures
+from test_desempeno.error_measures import error_measures, fix_particles_oustide
+
 
 poblaciones = []
 mean = np.array([20.7247332, 9.61818939])
@@ -42,6 +43,9 @@ t0 = time.time()
 df_ground_truth = generate_sequence(M = 512, N = 512, frames = 2, sigma_r = 4, poblaciones = poblaciones)
 
 df_ground_truth_filtered = df_ground_truth[df_ground_truth.intensity > 50] 
+
+#df_ground_truth_filtered = fix_particles_oustide(df_ground_truth_filtered, M = 512, N = 512)
+
 t1 = time.time()
 print('Finished running df_ground_truth in:', t1-t0)	
 
@@ -57,7 +61,7 @@ print('Finished running evaluation in:', t1-t0)
 print('Now computing the error measures')
 t0 = time.time()
 
-TP, FN, FP, JSC = error_measures(df_X=df_ground_truth, df_Y=df_detected, M=512, N=512, max_dist=30)
+TP, FN, FP, JSC = error_measures(df_X=df_ground_truth, df_Y=df_detected, M=512, N=512, max_dist=60)
 t1 = time.time()
 print('Finished running error_measures in:', t1-t0)	
 
@@ -66,7 +70,7 @@ print('False Negatives = ', FN)
 print('False Positives = ', FP)
 print('JSC =', JSC)
 
-print('df_ground_truth:')
-print(df_ground_truth)
+print('df_ground_truth_filtered:')
+print(df_ground_truth_filtered)
 print('df_detected:')
 print(df_detected)
