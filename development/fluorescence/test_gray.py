290cfedb93865d2e1805e84ece23342d6096e08b
Leo <leonardo.de.los.santos280796@gmail.com>
2020-03-10 23:33:12 -0300

    Updated: 10/03/2020 23:33:09.

--- development/fluorescence/test_gray.py
+++ development/fluorescence/test_gray.py
@@ -7,5 +7,5 @@ img = imread("Images_in/sample.jpg")
 image_bw = color.rgb2gray(img)
 grayscale = np.uint8(np.round(((image_bw - np.min(image_bw)) / (np.max(image_bw) - np.min(image_bw)) * 255)))
 
-print(image_bw*255)
+print(image_bw * 255)
 print(grayscale)
