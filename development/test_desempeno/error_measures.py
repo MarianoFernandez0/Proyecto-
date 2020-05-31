52a35f6edbf74da0f56df20f8bf29344e407559f
Mariano <mariano.ftr@gmail.com>
2020-05-30 21:22:22 -0300

    Agrego distance_between_two_tracks en error_measures.py

--- development/test_desempeno/error_measures.py
+++ development/test_desempeno/error_measures.py
@@ -35,21 +35,21 @@ def get_optimal_assignment(X, Y, max_dist):
     return Y_opt
 
 
-def error_measures(ground_truth_df, detected_df, M, N, max_dist):
-    '''
-    Parametros:
+def error_measures(ground_truth_df, detected_df, max_dist):
+    """
+    Parameters:
         ground_truth_df (df(id, x, y, total_pixels, mask, frame)): Coordenadas ground truth de las partículas.
         df_Y (df(id, x, y, total_pixels, mask, frame)): Coordenadas medidas de las partículas.
         M (int): Ancho de la imagen.
         N (int): Largo de la imagen.
         max_dist (int): Máxima distancia entre dos partículas para considerar que no son la misma.
 
-    Returwn:
+    Returns:
         TP (int): Verdaderos Positivos
         FN (int): Falsos Negativos
         FP (int): Falsos Positivos
         JSC (int): Índice de Jaccard
-    '''
+    """
 
     TP = 0
     FN = 0
@@ -76,29 +76,90 @@ def error_measures(ground_truth_df, detected_df, M, N, max_dist):
     return TP, FN, FP, JSC
 
 
-'''
-PRUEBA: 
+def distance_between_two_tracks(track_a, track_b, max_dist):
+    """
+    Toma como entrada dos trayectorias y calcula la distancia entre ellas.
+    Parameters:
+        track_a: dimensiones (N, 4), siendo sus comlumnas 1)posición x, 2)posición y 4)frame.
+        track_b: de la misma forma que track_x
+        max_dist: distancia máxima, si la distancia entre dos puntos es mayor a la distancia máxima se considera un
+                    error en la asignacion.
+    Returns:
+        distance (float): distancia entre las dos trayectorias.
+    """
+    min_frame_a = np.min(track_a[:, 4])
+    print('min_frame_x', min_frame_a)
+    min_frame_b = np.min(track_b[:, 4])
+    print('min_frame_y', min_frame_b)
+    min_frame = int(min(min_frame_a, min_frame_b))
+    print('min_frame', min_frame)
 
-N = 15
-F = 5
+    max_frame_a = np.max(track_a[:, 4])
+    max_frame_b = np.max(track_b[:, 4])
+    max_frame = int(max(max_frame_a, max_frame_b))
 
-X = np.zeros((N*F,3))
-Y = np.zeros((N*F,3))
+    distance = 0
+    for frame in range(min_frame, max_frame):
+        if frame < min_frame_a:
+            distance += max_dist
+        elif frame < min_frame_b:
+            distance += max_dist
+        elif frame > max_frame_a:
+            distance += max_dist
+        elif frame > max_frame_b:
+            distance += max_dist
+        else:
+            coord_track_a = track_a[track_a[:, 4] == frame][:, 1:3].squeeze()
+            coord_track_b = track_b[track_b[:, 4] == frame][:, 1:3].squeeze()
+            distance += np.sqrt((coord_track_a[0] - coord_track_b[0])**2 + (coord_track_a[1] - coord_track_b[1])**2)
+
+    return distance
 
 
-for n in range(N):
-	for f in range(F):
-		X[n*F+f,:] = [250+np.random.normal(0, 20), 250+np.random.normal(0, 20), f]
-		Y[n*F+f,:] = [500+np.random.normal(0, 20), 500+np.random.normal(0, 20), f]
+# ----------------------------------------------------------------------------------------------------------------------
+# PRUEBA:
 
-for i in range(30):
-	Y[i,:] = X[i,:]
-df_X = pd.DataFrame(X,columns = ['x','y','frame'])
-df_Y = pd.DataFrame(Y,columns = ['x','y','frame'])
+# N = 15
+# F = 5
 
-#print(df_X)
-#print(df_Y)
+# X = np.zeros((N*F,3))
+# Y = np.zeros((N*F,3))
 
-TP, FN, FP, JSC = error_measures(df_X, df_Y, 512, 512, 30)
-print(TP, FN, FP, JSC)
-'''
+
+# for n in range(N):
+# 	for f in range(F):
+# 		X[n*F+f,:] = [250+np.random.normal(0, 20), 250+np.random.normal(0, 20), f]
+# 		Y[n*F+f,:] = [500+np.random.normal(0, 20), 500+np.random.normal(0, 20), f]
+
+# for i in range(30):
+# 	Y[i,:] = X[i,:]
+# df_X = pd.DataFrame(X,columns = ['x','y','frame'])
+# df_Y = pd.DataFrame(Y,columns = ['x','y','frame'])
+
+# print(df_X)
+# print(df_Y)
+
+# TP, FN, FP, JSC = error_measures(df_X, df_Y, 512, 512, 30)
+# print(TP, FN, FP, JSC)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# tracks = pd.read_csv('tracks.csv')
+# print(tracks.head())
+# tracks = tracks.to_numpy()
+# print(tracks.shape)
+# track_a = tracks[tracks[:, 0] == 2]
+# track_a_new = np.zeros((track_a.shape[0], track_a.shape[1]+1))
+# track_a_new[:, :4] = track_a
+# track_a_new[:, 4] = track_a[:, 3]
+
+# print('track_a_new', track_a_new.shape)
+# track_b = tracks[tracks[:, 0] == 3]
+
+# track_b_new = np.zeros((track_b.shape[0], track_b.shape[1]+1))
+# track_b_new[:, :4] = track_b
+# track_b_new[:, 4] = track_b[:, 3]
+# print(track_a.shape)
+# print(track_b.shape)
+# dist = distance_between_two_tracks(track_a_new, track_b_new, 0)
+# print(dist)
+# ----------------------------------------------------------------------------------------------------------------------
