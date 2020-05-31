041ba0b492ba880939cddcc3c67145e881ac1a50
Leo <leonardo.de.los.santos280796@gmail.com>
2020-05-31 15:34:50 -0300

    Updated: 31/05/2020 15:34:45.

---
+++ tracking_algorithm_evaluation/python_plus_c++/README.md
@@ -0,0 +1,11 @@
+# Python_plus_C++
+
+This module integrates the detectections from a software developed in Python, 
+and with a bash script run a C++ program to make the tracking, using JPDAF implementation.
+
+In command line:
+```
+python pdaf_implementation
+```
+
+The output is saved in current directory.
---
+++ tracking_algorithm_evaluation/python_plus_c++/main_jpdaf.py
@@ -0,0 +1,38 @@
+from functions.evaluation import evaluation
+from functions.change_format_for_jpdaf import write_csv_for_jpdaf, save_secuence_as_jpgs_for_jpdaf
+import tifffile
+import os
+from subprocess import call
+
+def main_jpdaf_implementation(video_sequence_path):
+    '''
+    Main of the implementation.
+    To run it, just call the "jpdaf_implementation.py arg1 arg2" from the command line.
+        arguments (arg1, arg2..) are:
+         the path for the sequence
+         the path of the csv output
+         the path for the images jpg
+    '''
+
+    video_sequence = os.listdir(video_sequence_path)[0]
+    print(os.listdir(video_sequence_path))
+    #if len(os.listdir(video_sequence_path)) > 1:
+    #    print("Must have only one sequence in directory")
+    #    return 1
+
+    print('Sequence name %s' % video_sequence)
+    tiff = tifffile.TiffFile(video_sequences_path + '/' + video_sequence)
+    detected = evaluation(tiff, include_mask=True)
+
+    folder_directory = 'sequences_for_jpdaf/'
+    save_secuence_as_jpgs_for_jpdaf(tiff, folder_directory)
+    write_csv_for_jpdaf(detected, folder_directory)
+    return 0
+
+
+########################################################
+#  START
+########################################################
+
+video_sequences_path = "./data_in/sequences"
+call("./execute_jpdaf.sh") if main_jpdaf_implementation(video_sequences_path) == 0 else print("error")
