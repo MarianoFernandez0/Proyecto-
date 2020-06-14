b19a7c8954aaafd9ef4fdb212ad9326683a5d8d9
Mariano <mariano.ftr@gmail.com>
2020-06-14 17:06:38 -0300

    trackmate errors

--- tracking_algorithm_evaluation/compute_errors_trackmate.py
+++ tracking_algorithm_evaluation/compute_errors_trackmate.py
@@ -4,28 +4,32 @@ import pandas as pd
 
 performance_measures = pd.DataFrame()
 
-dataset_dir = 'python_plus_octave/data'
-frame_rate_list = os.listdir(dataset_dir)
+dataset_dir = 'Trackmate'
+datasets_list = os.listdir(dataset_dir)
 
-for frame_rate in frame_rate_list:
-    if 'Hz' in frame_rate:
-        gt_dir = dataset_dir+'/'+frame_rate+'/'+'dataset_'+frame_rate[8]+'_data.csv'
-        gt_tracks = pd.read_csv(gt_dir)
-        print('gt_dir', gt_dir)
-        algoritmns = os.listdir(dataset_dir+'/'+frame_rate)
-        for algoritmn in algoritmns:
-            if 'tracking' in algoritmn:
-                tracks_dir = dataset_dir+'/'+frame_rate+'/'+algoritmn+'/dataset_'+frame_rate[8]+'/'+'tracks.csv'
-                tracks_csv = pd.read_csv(tracks_dir)
-                print(tracks_dir)
-                tracks_csv = tracks_csv[tracks_csv['frame'] < tracks_csv['frame'].max()]
-                gt_tracks = gt_tracks[gt_tracks['frame'] < tracks_csv['frame'].max()]
+for dataset_folder in datasets_list:
+    dataset_num = dataset_folder[7]
+    dataset_frec = dataset_folder[9:11]
+    gt_dir = dataset_dir + '/' + dataset_folder + '/' + 'dataset_' + dataset_num + '_' + dataset_frec + '.csv'
+    print('gt_dir', gt_dir)
+    gt_tracks = pd.read_csv(gt_dir)
+    tracks_dir = (dataset_dir + '/' + dataset_folder + '/' + 'Spots in tracks statistics-dataset' + dataset_num + '-' +
+                  dataset_frec + 'Hz.csv')
+    print(tracks_dir)
+    tracks_csv = pd.read_csv(tracks_dir)
 
-                gt_tracks = gt_tracks[gt_tracks['frame'] > 1]
+    tracks_csv.rename(columns={'TRACK_ID': 'id',
+                               'POSITION_X': 'x',
+                               'POSITION_Y': 'y',
+                               'FRAME': 'frame'}, inplace=True)
+    tracks_csv = tracks_csv[tracks_csv['frame'] < tracks_csv['frame'].max()]
 
-                gt_tracks.rename(columns={'id_particle': 'id'}, inplace=True)
-                error = track_set_error(gt_tracks, tracks_csv, 40)
-                error = pd.DataFrame.from_dict(error, orient='index', columns=[frame_rate+'_'+algoritmn])
-                error = error.transpose()
-                performance_measures = pd.concat((performance_measures, error))
-performance_measures.to_csv('performance_measures_octave.csv')
+    gt_tracks = gt_tracks[gt_tracks['frame'] < tracks_csv['frame'].max()]
+    gt_tracks = gt_tracks[gt_tracks['frame'] > 1]
+    gt_tracks.rename(columns={'id_particle': 'id'}, inplace=True)
+
+    error = track_set_error(gt_tracks, tracks_csv, 40)
+    error = pd.DataFrame.from_dict(error, orient='index', columns=[frame_rate+'_'+algoritmn])
+    error = error.transpose()
+    performance_measures = pd.concat((performance_measures, error))
+performance_measures.to_csv('performance_measures_trackmate.csv')
