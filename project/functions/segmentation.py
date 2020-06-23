f1b2fc50583960517839f5977229ffef075c2588
Leo <leonardo.de.los.santos280796@gmail.com>
2020-06-22 23:55:06 -0300

    Segmentation changed

--- project/functions/segmentation.py
+++ project/functions/segmentation.py
@@ -1,7 +1,5 @@
 #!/home/leo/anaconda3/bin/python
-import skimage.filters as filters
-import skimage.color as color
-import scipy.ndimage as nd
+import cv2
 import numpy as np
 
 
@@ -16,8 +14,9 @@ def segmentation(image):
     Salida:
         Máscara de tamaño NxMx1 con la imagen segmentada
     '''
-    image_bw = color.rgb2gray(image)
-    image_filtered = nd.gaussian_filter(image_bw, sigma=3)
-    image_threshold = filters.threshold_otsu(image_filtered)
-    mask = (image_bw > image_threshold) * 255
-    return mask.astype(np.uint8)
+    image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, image_tresh = cv2.threshold(image_bw, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    image_streched = cv2.morphologyEx(image_tresh, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+    mask = cv2.dilate(image_streched, np.ones((3, 3), np.uint8), iterations=1)
+
+    return mask
