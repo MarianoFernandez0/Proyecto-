8e1c4d0369f6312d5b98df2ccfc37eac056a7562
Mariano <mariano.ftr@gmail.com>
2020-08-29 16:49:52 -0300

    plots

--- tracking_algorithm_evaluation/compute_errors_urbano.py
+++ tracking_algorithm_evaluation/compute_errors_urbano.py
@@ -9,10 +9,11 @@ import argparse
 import json
 from tqdm import tqdm
 
+ALGORITHMS = ['NN', 'GNN', 'PDAF', 'JPDAF', 'ENNJPDAF']
+
 
 def compute_errors(dataset_dir, results_file, save_vid=True):
     performance_measures = pd.DataFrame()
-    algorithms = ['NN', 'GNN', 'PDAF', 'JPDAF', 'ENNJPDAF']
 
     print('dataset_dir: ', dataset_dir)
     sub_dataset_dir = os.path.join(dataset_dir.split('/')[1], dataset_dir.split('/')[2])
@@ -34,7 +35,7 @@ def compute_errors(dataset_dir, results_file, save_vid=True):
         os.makedirs(videos_dir, exist_ok=True)
         frame_rate = config['Input']['fps']
         tracks_file = config['Output']['tracks_csv']
-        algorithm = algorithms[int(config['Algorithm params']['mtt_algorithm']) - 1]
+        algorithm = ALGORITHMS[int(config['Algorithm params']['mtt_algorithm']) - 1]
 
         gt_files = os.listdir(os.path.join(dataset_dir, sub_dataset_dir, 'datasets/data_sequence'))
         gt_file = [file for file in gt_files if str(frame_rate) + 'Hz' in file][0]
@@ -62,7 +63,6 @@ def compute_errors(dataset_dir, results_file, save_vid=True):
             print("\n video_file_out ", video_file_out)
 
         # compute error measures
-
         error = track_set_error(gt_tracks, tracks, max_dist=40)
         col_name = str(frame_rate) + 'Hz_' + algorithm
         error = pd.DataFrame.from_dict(error, orient='index', columns=[col_name])
@@ -73,29 +73,29 @@ def compute_errors(dataset_dir, results_file, save_vid=True):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Compute tracking performance measures')
-    parser.add_argument('--dataset_dir', default='python_plus_octave/datasets_25_08_2020/dataset_1',
-                        help='directory of the dataset')
-    parser.add_argument('--results_file', default='error_results/dataset_performance_measures_results.csv',
-                        help='file to save performance results')
-    parser.add_argument('--save_vid', default=True, help='Flag')
-    args = parser.parse_args()
-    compute_errors(args.dataset_dir, args.results_file, args.save_vid)
+    # parser = argparse.ArgumentParser(description='Compute tracking performance measures')
+    # parser.add_argument('--dataset_dir', default='python_plus_octave/datasets_25_08_2020/dataset_1',
+    #                     help='directory of the dataset')
+    # parser.add_argument('--results_file', default='error_results/dataset_performance_measures_results.csv',
+    #                     help='file to save performance results')
+    # parser.add_argument('--save_vid', default=True, help='Flag')
+    # args = parser.parse_args()
+    # compute_errors(args.dataset_dir, args.results_file, args.save_vid)
 
-    # DATASET_DIRS = ["python_plus_octave/datasets_25_08_2020/dataset_1",
-    #                 "python_plus_octave/datasets_25_08_2020/dataset_2",
-    #                 "python_plus_octave/datasets_25_08_2020/dataset_3",
-    #                 "python_plus_octave/datasets_25_08_2020/dataset_4",
-    #                 "python_plus_octave/datasets_25_08_2020/dataset_5",
-    #                 "python_plus_octave/datasets_25_08_2020/dataset_6"]
-    #
-    # RESULTS_FILES = ["error_results/datasets_25_08_2020/dataset_1_performance_measures.csv",
-    #                   "error_results/datasets_25_08_2020/dataset_2_performance_measures.csv",
-    #                   "error_results/datasets_25_08_2020/dataset_3_performance_measures.csv",
-    #                   "error_results/datasets_25_08_2020/dataset_4_performance_measures.csv",
-    #                   "error_results/datasets_25_08_2020/dataset_5_performance_measures.csv",
-    #                   "error_results/datasets_25_08_2020/dataset_6_performance_measures.csv"]
+    DATASET_DIRS = ["python_plus_octave/datasets_25_08_2020/dataset_1",
+                    "python_plus_octave/datasets_25_08_2020/dataset_2",
+                    "python_plus_octave/datasets_25_08_2020/dataset_3",
+                    "python_plus_octave/datasets_25_08_2020/dataset_4",
+                    "python_plus_octave/datasets_25_08_2020/dataset_5",
+                    "python_plus_octave/datasets_25_08_2020/dataset_6"]
 
-    # for i, dataset in enumerate(DATASET_DIRS):
-    #     results = RESULTS_FILES[i]
-    #     compute_errors(dataset, results)
+    RESULTS_FILES = ["error_results/datasets_25_08_2020/dataset_1_performance_measures.csv",
+                      "error_results/datasets_25_08_2020/dataset_2_performance_measures.csv",
+                      "error_results/datasets_25_08_2020/dataset_3_performance_measures.csv",
+                      "error_results/datasets_25_08_2020/dataset_4_performance_measures.csv",
+                      "error_results/datasets_25_08_2020/dataset_5_performance_measures.csv",
+                      "error_results/datasets_25_08_2020/dataset_6_performance_measures.csv"]
+
+    for i, dataset in enumerate(DATASET_DIRS):
+        results = RESULTS_FILES[i]
+        compute_errors(dataset, results)
